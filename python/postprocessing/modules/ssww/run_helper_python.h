210436d43da61cccf8506ecd27a005b8d1c083dd
freejiebao <jiexiao@pku.edu.cn>
2019-11-12 17:23:43 +0100

    update

--- python/postprocessing/modules/ssww/run_helper_python.h
+++ python/postprocessing/modules/ssww/run_helper_python.h
@@ -27,33 +27,34 @@ using rvec_i = const RVec<int> &;
 
 // https://github.com/PKUHEPEWK/WGamma/blob/master/wg_rdf.py
 // lepton sf file
-TFile electron_id_2016_sf_file("../../data/leptonSF/electron/2016/2016LegacyReReco_ElectronMVA80_Fall17V2.root","read");
+TFile electron_id_2016_sf_file("../../data/leptonSF/2016/electron/2016LegacyReReco_ElectronMVA80_Fall17V2.root","read");
 TH2F * electron_id_2016_sf = (TH2F*) electron_id_2016_sf_file.Get("EGamma_SF2D");
-TFile electron_id_2017_sf_file("../../data/leptonSF/electron/2017/2017_ElectronMedium.root","read");
+TFile electron_id_2017_sf_file("../../data/leptonSF/2017/electron/2017_ElectronMedium.root","read");
 TH2F * electron_id_2017_sf = (TH2F*)electron_id_2017_sf_file.Get("EGamma_SF2D");
-TFile electron_id_2018_sf_file("../../data/leptonSF/electron/2018/2018_ElectronMedium.root","read");
+TFile electron_id_2018_sf_file("../../data/leptonSF/2018/electron/2018_ElectronMedium.root","read");
 TH2F * electron_id_2018_sf = (TH2F*)electron_id_2018_sf_file.Get("EGamma_SF2D");
-TFile electron_reco_2016_sf_file("../../data/leptonSF/electron/2016/EGM2D_BtoH_GT20GeV_RecoSF_Legacy2016.root","read");
+TFile electron_reco_2016_sf_file("../../data/leptonSF/2016/electron/EGM2D_BtoH_GT20GeV_RecoSF_Legacy2016.root","read");
 TH2F * electron_reco_2016_sf = (TH2F*) electron_reco_2016_sf_file.Get("EGamma_SF2D");
-TFile electron_reco_2017_sf_file("../../data/leptonSF/electron/2017/egammaEffi.txt_EGM2D_runBCDEF_passingRECO.root","read");
+TFile electron_reco_2017_sf_file("../../data/leptonSF/2017/electron/egammaEffi.txt_EGM2D_runBCDEF_passingRECO.root","read");
 TH2F * electron_reco_2017_sf = (TH2F*)electron_reco_2017_sf_file.Get("EGamma_SF2D");
-TFile electron_reco_2018_sf_file("../../data/leptonSF/electron/2018/egammaEffi.txt_EGM2D_updatedAll.root" ,"read");
+TFile electron_reco_2018_sf_file("../../data/leptonSF/2018/electron/egammaEffi.txt_EGM2D_updatedAll.root" ,"read");
 TH2F * electron_reco_2018_sf = (TH2F*)electron_reco_2018_sf_file.Get("EGamma_SF2D");
 
-TFile muon_iso_2016_sf_file("../../data/leptonSF/muon/2016/RunBCDEF_SF_ISO_tight.root","read");
+TFile muon_iso_2016_sf_file("../../data/leptonSF/2016/muon/RunBCDEF_SF_ISO_tight.root","read");
 TH2D * muon_iso_2016_sf = (TH2D*) muon_iso_2016_sf_file.Get("NUM_TightRelIso_DEN_TightIDandIPCut_eta_pt");
-TFile muon_id_2016_sf_file("../../data/leptonSF/muon/2016/RunBCDEF_SF_ID.root","read");
+TFile muon_id_2016_sf_file("../../data/leptonSF/2016/muon/RunBCDEF_SF_ID.root","read");
 TH2D * muon_id_2016_sf = (TH2D*) muon_id_2016_sf_file.Get("NUM_TightID_DEN_genTracks_eta_pt");
-TFile muon_iso_2017_sf_file("../../data/leptonSF/electron/2017/RunBCDEF_SF_ISO.root","read");
+TFile muon_iso_2017_sf_file("../../data/leptonSF/2017/electron/RunBCDEF_SF_ISO.root","read");
 TH2D * muon_iso_2017_sf = (TH2D*) muon_iso_2017_sf_file.Get("NUM_TightRelIso_DEN_TightIDandIPCut_pt_abseta");
-TFile muon_id_2017_sf_file("../../data/leptonSF/electron/2017/RunBCDEF_SF_ID.root","read");
+TFile muon_id_2017_sf_file("../../data/leptonSF/2017/electron/RunBCDEF_SF_ID.root","read");
 TH2D * muon_id_2017_sf = (TH2D*) muon_id_2017_sf_file.Get("NUM_TightID_DEN_genTracks_pt_abseta");
-TFile muon_iso_2018_sf_file("../../data/leptonSF/electron/2018/RunABCD_SF_ISO.root","read");
+TFile muon_iso_2018_sf_file("../../data/leptonSF/2018/electron/RunABCD_SF_ISO.root","read");
 TH2D * muon_iso_2018_sf = (TH2D*) muon_iso_2018_sf_file.Get("NUM_TightRelIso_DEN_TightIDandIPCut_pt_abseta");
-TFile muon_id_2018_sf_file("../../data/leptonSF/electron/2018/RunABCD_SF_ID.root","read");
+TFile muon_id_2018_sf_file("../../data/leptonSF/2018/electron/RunABCD_SF_ID.root","read");
 TH2D * muon_id_2018_sf = (TH2D*)muon_id_2018_sf_file.Get("NUM_TightID_DEN_TrackerMuons_pt_abseta");
 
 // fake lepton weight files
+/*
 TFile muon_2016_file("/afs/cern.ch/user/a/amlevin/wg/fake_lepton_weights/muon_2016_frs.root");
 TFile electron_2016_file("/afs/cern.ch/user/a/amlevin/wg/fake_lepton_weights/electron_2016_frs.root");
 TFile muon_2017_file("/afs/cern.ch/user/a/amlevin/wg/fake_lepton_weights/muon_2017_frs.root");
@@ -66,7 +67,7 @@ TH2D * muon_2017_fr_hist = (TH2D*)muon_2017_file.Get("muon_frs");
 TH2D * electron_2017_fr_hist = (TH2D*)electron_2017_file.Get("electron_frs");
 TH2D * muon_2018_fr_hist = (TH2D*)muon_2018_file.Get("muon_frs");
 TH2D * electron_2018_fr_hist = (TH2D*)electron_2018_file.Get("electron_frs");
-
+*/
 RVec<float> electron_efficiency_scale_factor(rvec_f pt, rvec_f eta, string year,bool id_err_up=false, bool reco_err_up=false) {
     TH2F * electron_reco_sf = 0;
     TH2F * electron_id_sf = 0;
@@ -155,7 +156,7 @@ RVec<float> muon_efficiency_scale_factor(rvec_f pt,rvec_f eta,string year,bool i
     }
     return sf;
 }
-
+/*
 RVec<float> get_fake_lepton_weight(rvec_f eta, rvec_f pt, string year, int lepton_pdg_id, string syst = "nominal")
 {
     TH2D * fr_hist = 0;
@@ -180,7 +181,7 @@ RVec<float> get_fake_lepton_weight(rvec_f eta, rvec_f pt, string year, int lepto
     }
     return wgt;
 }
-
+*/
 float calc_mjj(float j1_pt, float j1_eta, float j1_phi, float j1_mass, float j2_pt, float j2_eta, float j2_phi, float j2_mass)
 {
     ROOT::Math::PtEtaPhiMVector p1(j1_pt, j1_eta, j1_phi, j1_mass);
