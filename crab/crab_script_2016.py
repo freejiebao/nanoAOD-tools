21b15c0e5bc5804a01422be14c22cc2f239eeeab
freejiebao <jiexiao@pku.edu.cn>
2020-01-08 05:43:50 +0100

    update

--- crab/crab_script_2016.py
+++ crab/crab_script_2016.py
@@ -19,7 +19,7 @@ from  PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetHelperRun2_ssww
 jmeCorrections = createJMECorrector(True, "2016", "B", "Total", True, "AK4PFchs", False)
 
 #p=PostProcessor(".",inputFiles(),None,os.environ['CMSSW_BASE']+"/python/PhysicsTools/NanoAODTools/postprocessing/scripts/ssww_keep_and_drop_2016.txt",modules=[countHistogramsModule(),PrefCorr_2016(),sswwModule2016(),muonScaleRes2016(),puWeight_2016(),jmeCorrections(),btagSF2016(),lepSF_2016()],provenance=True,justcount=False,noOut=False,fwkJobReport=True,outputbranchsel = os.environ['CMSSW_BASE']+"/python/PhysicsTools/NanoAODTools/postprocessing/scripts/ssww_output_branch_selection_2016.txt")
-p=PostProcessor(".",inputFiles(),None,os.environ['CMSSW_BASE']+"/python/PhysicsTools/NanoAODTools/postprocessing/scripts/ssww_keep_and_drop_2016.txt",modules=[countHistogramsModule(),puWeight_2016(),PrefCorr_2016(),jmeCorrections(),sswwModule2016(),muonScaleRes2016(),btagSF2016()],provenance=True,justcount=False,noOut=False,fwkJobReport=True,outputbranchsel = os.environ['CMSSW_BASE']+"/python/PhysicsTools/NanoAODTools/postprocessing/scripts/ssww_output_branch_selection_2016.txt")
+p=PostProcessor(".",inputFiles(),None,os.environ['CMSSW_BASE']+"/python/PhysicsTools/NanoAODTools/postprocessing/scripts/ssww_keep_and_drop_2016.txt",modules=[countHistogramsModule(),puWeight_2016(),PrefCorr_2016(),sswwModule2016(),muonScaleRes2016(),jmeCorrections(),btagSF2016()],provenance=True,justcount=False,noOut=False,fwkJobReport=True,outputbranchsel = os.environ['CMSSW_BASE']+"/python/PhysicsTools/NanoAODTools/postprocessing/scripts/ssww_output_branch_selection_2016.txt")
 p.run()
 
 print "DONE"
